03b7036980b05cbc85a458dc3f6353e3c4beaca0
Victor Manuel Villamil Romero <n10080236@qut.edu.au>
2019-03-03 18:46:25 +1000

    Changes  3rd of March

--- Jet_Lag_Project.py
+++ Jet_Lag_Project.py
@@ -7,19 +7,32 @@ Created on Sat Mar  2 15:21:31 2019
 
 import Wrangling_DataSource as WD
 
-
 def main():
-    #Defining the range of years to be evaluated.
-    min, max = 2017,2019
+    #Defining the global variables 
+    min, max = 1950,1952
+    teamsDf = {}
+    teamsPerYear = {}
     
-    #Using the class Weangling DataSource.
-    dataImported = WD.Wrangling_DataSource(min,max)    
-    dictOfUrls = dataImported.createRange()    
-    for year,url_Des in dictOfUrls.items():
-        dataImported.getData(url_Des[0],url_Des[1])
+    #USING THE CLASS WRANGLING DATASOURCE
+    dataImported = WD.Wrangling_DataSource(min,max)  
+    
+    #Creating Dictionary of url and destination
+    dictUrlsDest = dataImported.createUrlDest()  
+    
+    ##Downloading and storing data 
+    for year,url_Des in dictUrlsDest.items():
+        dataImported.getData(url_WD= url_Des[0],destination_WD= url_Des[1])
         print("Data from " + str(year) + " imported and stored inside of" + str(url_Des[1]))
     
+    #Pre-Processing file Teams per each year.
+    for year in dictUrlsDest.keys():    
+        teamsDf[year] = dataImported.getTeams(year_WD= year) 
+        teamsPerYear[year] = dataImported.getTeamsPerYear(year_WD= year, teamsDf_WD= teamsDf)
     
+    for k,v in teamsPerYear.items():
+        print(str(k) + " => " + str(v))
+        
+        
     
 
 #Executing the main program.
--- Wrangling_DataSource.py
+++ Wrangling_DataSource.py
@@ -4,22 +4,53 @@ Created on Sat Mar  2 14:48:58 2019
 
 @author: corve
 """
-import requests, zipfile, io
+import requests, zipfile, io, os, glob
+#The glob module finds all the pathnames matching a specified pattern
+#The requests module allows you to send organic, grass-fed HTTP/1.1 requests, without the need for manual labor
+#
+
+import pandas as pd 
+
 
 class Wrangling_DataSource():
     
     def __init__(self,minrange,maxrange):
         self.minrange = minrange
-        self.maxrange = maxrange     
+        self.maxrange = maxrange
+        self.strRef = ["https://www.retrosheet.org/events/",#Webpage data origin
+                       "eve.zip",#Extension file zip to download
+                       ".\Resources\\",#local folder to store the data
+                       "TEAM",#Name of the main file in the zip folder
+                       ["ID","First Letter","City","Team Name"]]#Labels from TEAM file.
+        self.years = list(range(self.minrange,self.maxrange))
     
-    def createRange(self):
-        years = list(range(self.minrange,self.maxrange))
+    def createUrlDest(self):        
         url = {}
-        for year in years:
-            url[year]=  ("https://www.retrosheet.org/events/" + str(year) + "eve.zip", ".\Resources\\" + str(year))
+        for year in self.years:
+            url[year]=  (self.strRef[0] + str(year) + self.strRef[1] , self.strRef[2] + str(year))
         return url
 
-    def getData(self,url,destination):
-        r = requests.get(url)
+    def getData(self,url_WD,destination_WD):
+        r = requests.get(url_WD)
         z = zipfile.ZipFile(io.BytesIO(r.content))
-        z.extractall(destination)
+        z.extractall(destination_WD)
+    
+    def getTeams(self,year_WD):
+        listString = []
+        path = self.strRef[2] + str(year_WD)
+        for infile in glob.glob(os.path.join(path, self.strRef[3] + str(year_WD) + "*")):
+            stringFile = open(infile, 'r').read()
+            for i in range(0,len(stringFile.splitlines())):
+                listString.append(stringFile.splitlines()[i].split(","))
+            df = pd.DataFrame.from_records(listString, columns = self.strRef[4] )
+        return df 
+    
+    def getTeamsPerYear(self,year_WD,teamsDf_WD):
+        teamsPerYear = teamsDf_WD[year_WD]["ID"].tolist() 
+        return teamsPerYear
+        
+    def getEva(self,year):
+        return
+        
+    def getRos(self,year):
+        return
