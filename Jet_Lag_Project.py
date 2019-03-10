885675ef0ba40be921b8d9e5713608fabce47b15
Victor Manuel Villamil Romero <n10080236@qut.edu.au>
2019-03-10 18:47:34 +1000

    Update py files

--- Jet_Lag_Project.py
+++ Jet_Lag_Project.py
@@ -7,11 +7,14 @@ Created on Sat Mar  2 15:21:31 2019
 
 import Wrangling_DataSource as WD
 
+teamsDf = {}
+teamsPerYear = {}
+filesToExtract = {}
+eventGames = {}
+
 def main():
     #Defining the global variables 
     min, max = 1950,1952
-    teamsDf = {}
-    teamsPerYear = {}
     
     #USING THE CLASS WRANGLING DATASOURCE
     dataImported = WD.Wrangling_DataSource(min,max)  
@@ -20,22 +23,25 @@ def main():
     dictUrlsDest = dataImported.createUrlDest()  
     
     ##Downloading and storing data 
-    for year,url_Des in dictUrlsDest.items():
-        dataImported.getData(url_WD= url_Des[0],destination_WD= url_Des[1])
-        print("Data from " + str(year) + " imported and stored inside of" + str(url_Des[1]))
+    for year,url_Des in dictUrlsDest.items(): dataImported.getData(url_WD= url_Des[0],destination_WD= url_Des[1])
     
     #Pre-Processing file Teams per each year.
-    for year in dictUrlsDest.keys():    
-        teamsDf[year] = dataImported.getTeams(year_WD= year) 
-        teamsPerYear[year] = dataImported.getTeamsPerYear(year_WD= year, teamsDf_WD= teamsDf)
+    for year in dictUrlsDest.keys(): teamsDf[year] = dataImported.getTeams(year_WD= year)
+    for year in dictUrlsDest.keys(): teamsPerYear[year] = dataImported.getTeamsPerYear(year_WD= year, teamsDf_WD= teamsDf)
     
-    for k,v in teamsPerYear.items():
-        print(str(k) + " => " + str(v))
-        
-        
+    #Creating Structure of List of list of Files from each season. [[EVA and EVN],[ROS],[EDA and EDN]]
+    for year in teamsPerYear.keys(): filesToExtract[year] = dataImported.getNameFiles(year_WD= year, teamsPerYear_WD= teamsPerYear[year] )  
     
+    #Extracting EVA and EVN files    
+    for year in filesToExtract.keys(): eventGames[year] = dataImported.getEvaEvnData(year_WD= year, filesToExtract_WD= filesToExtract[year]) 
 
 #Executing the main program.
-if __name__ == "__main__": 
-    main()
-        
+#if __name__ == "__main__": 
+main()
+
+
+    
+with open('test.csv', 'w') as f:
+    for key in eventGames[1950].keys():
+        for idx in eventGames[1950][key].index.values:
+            f.write("%s\n"%(",".join(list(eventGames[1950][key].loc[[idx], :]))))
--- Wrangling_DataSource.py
+++ Wrangling_DataSource.py
@@ -5,11 +5,9 @@ Created on Sat Mar  2 14:48:58 2019
 @author: corve
 """
 import requests, zipfile, io, os, glob
+import pandas as pd 
 #The glob module finds all the pathnames matching a specified pattern
 #The requests module allows you to send organic, grass-fed HTTP/1.1 requests, without the need for manual labor
-#
-
-import pandas as pd 
 
 
 class Wrangling_DataSource():
@@ -21,7 +19,7 @@ class Wrangling_DataSource():
                        "eve.zip",#Extension file zip to download
                        ".\Resources\\",#local folder to store the data
                        "TEAM",#Name of the main file in the zip folder
-                       ["ID","First Letter","City","Team Name"]]#Labels from TEAM file.
+                       ["teamID","league","city","teamname"]]#Labels from TEAM file.
         self.years = list(range(self.minrange,self.maxrange))
     
     def createUrlDest(self):        
@@ -35,6 +33,9 @@ class Wrangling_DataSource():
         z = zipfile.ZipFile(io.BytesIO(r.content))
         z.extractall(destination_WD)
     
+    def getDirLength(self, destination_WD):
+        return len(os.listdir(destination_WD))       
+    
     def getTeams(self,year_WD):
         listString = []
         path = self.strRef[2] + str(year_WD)
@@ -46,11 +47,98 @@ class Wrangling_DataSource():
         return df 
     
     def getTeamsPerYear(self,year_WD,teamsDf_WD):
-        teamsPerYear = teamsDf_WD[year_WD]["ID"].tolist() 
+        teamsPerYear = teamsDf_WD[year_WD][[self.strRef[4][0],self.strRef[4][1]]]        
         return teamsPerYear
         
-    def getEva(self,year):
-        return
+    def getNameFiles(self,year_WD, teamsPerYear_WD): 
+        filesList = []
+        teamsPerYear_WD["evxFiles"] = str(year_WD) + teamsPerYear_WD.loc[:,"teamID"] + ".EV" + teamsPerYear_WD.loc[:,"league"] 
+        teamsPerYear_WD["rosFiles"] = teamsPerYear_WD.loc[:,"teamID"] + str(year_WD) + ".ROS"
+        filesList.append(list(teamsPerYear_WD["evxFiles"]))
+        filesList.append(list(teamsPerYear_WD["rosFiles"]))        
+        filesList.append([str(year_WD) + ".EDA", str(year_WD) + ".EDN"])
+        return filesList
         
-    def getRos(self,year):
-        return
+    def getEvaEvnData(self,year_WD, filesToExtract_WD):       
+        dicEvaEvnData = {}
+        for file in filesToExtract_WD[0]:
+            print(str(file) + " file in process.")
+            index = []
+            infocol = []
+            dictemp = {}
+            #Extracting the columns and the index for the dataframe
+            f = open(self.strRef[2] + str(year_WD)+ "\\" + file , 'r')
+            for line in f:
+                line = line.strip().split(",")
+                if line[0]=="id":   index.append(line[1])
+                if line[0]=="info" and line[1] not in infocol: infocol.append(line[1])     
+            f.close()
+            
+            #Adding Columns for start data.
+            infocol.extend(["start_playerid", 
+                            "start_playersname", 
+                            "start_visithometeam", 
+                            "start_battingposition", 
+                            "start_fieldingposition"])
+            
+            #Adding Columns for sub data.
+            infocol.extend(["sub_playerid", 
+                            "sub_playersname", 
+                            "sub_homevisitor", 
+                            "sub_battingposition", 
+                            "sub_fieldingposition"])
+            
+            #Adding Columns for play data.
+            infocol.extend(["play_inning", 
+                            "play_homevisitor", 
+                            "play_playerid", 
+                            "play_count", 
+                            "play_pitches",
+                            "play_event"])
+            
+            dftemp = pd.DataFrame(columns= infocol , index= index)
+            
+            #Extracting the data from info.
+            g = open(self.strRef[2] + str(year_WD) + "\\" + file , 'r')        
+            for line in g:
+                line = line.strip().split(",")
+                if line[0]=="id":   guide = line[1]  
+                
+                if line[0]=="info":
+                    dictemp[line[1]] = line[2]
+                    dftemp.loc[guide] = pd.Series(dictemp)
+                    
+                if line[0]=="start":
+                    dictemp["start_playerid"] = line[1]
+                    dictemp["start_playersname"] = line[2]
+                    dictemp["start_visithometeam"] = line[3]
+                    dictemp["start_battingposition"] = line[4]
+                    dictemp["start_fieldingposition"] = line[5]
+                    dftemp.loc[guide] = pd.Series(dictemp)
+                    
+                if line[0]=="play":
+                    dictemp["play_inning"] = line[1]
+                    dictemp["play_homevisitor"] = line[2]
+                    dictemp["play_playerid"] = line[3]
+                    dictemp["play_count"] = line[4]
+                    dictemp["play_pitches"] = line[5]
+                    dictemp["play_event"] = line[6]
+                    dftemp.loc[guide] = pd.Series(dictemp)
+                
+                if line[0]=="sub":
+                    dictemp["sub_playerid"] = line[1]
+                    dictemp["sub_playersname"] = line[2]
+                    dictemp["sub_homevisitor"] = line[3]
+                    dictemp["sub_battingposition"] = line[4]
+                    dictemp["sub_fieldingposition"] = line[5]
+                    dftemp.loc[guide] = pd.Series(dictemp)                
+            g.close()
+            
+            dicEvaEvnData[file] = dftemp
+        
+        return dicEvaEvnData
+    
+    def getRosData():
+        dicRosData = {}
+        
+        return dicRosData
